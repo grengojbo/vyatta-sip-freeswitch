3c72b9e71027c8fa7c71f41103165e193c6f0a17
Oleg Dolya <oleg.dolya@gmail.com>
2011-05-19 12:46:40 +0300

    modified:   Makefile.am new file:   scripts/vyatta-freeswitch-vars modified:   scripts/vyatta-freeswitch.pl modified:   templates-cfg/service/sip/areacode/node.def modified:   templates-cfg/service/sip/country/node.def new file:   templates-cfg/service/sip/default-language/node.def modified:   templates-cfg/service/sip/domain-name/node.def modified:   templates-cfg/service/sip/dump-cores/node.def modified:   templates-cfg/service/sip/language/node.def modified:   templates-cfg/service/sip/loglevel/node.def modified:   templates-cfg/service/sip/max-sessions/node.def modified:   templates-cfg/service/sip/node.def modified:   templates-cfg/service/sip/sessions-per-second/node.def modified:   templates-cfg/service/sip/zrtp-secure-media/node.def

--- scripts/vyatta-freeswitch.pl
+++ scripts/vyatta-freeswitch.pl
@@ -43,26 +43,94 @@ use Data::Dumper;
 use strict;
 use warnings;
 
+my $fs_conf_dir = '/opt/freeswitch/conf';
+
+my ($conf_name, $show_names);
+
 sub usage {
     print <<EOF;
-Usage: $0 --dev=<interface> --check=<type>
-       $0 --dev=<interface> --warn
-       $0 --dev=<interface> --valid-mac=<aa:aa:aa:aa:aa:aa>
-       $0 --dev=<interface> --valid-addr-commit={addr1 addr2 ...}
-       $0 --dev=<interface> --speed-duplex=speed,duplex
-       $0 --dev=<interface> --check-speed=speed,duplex
-       $0 --dev=<interface> --allowed-speed
-       $0 --dev=<interface> --isup
+Usage: $0 --conf=<conf_name>
        $0 --show=<type>
 EOF
     exit 1;
 }
-my $fs_conf_dir = '/opt/freeswitch/conf/freeswitch.xml';
-my $fs_config_new = XML::Simple->new(rootname=>'document',);
-my $fs_config = XMLin($fs_conf_dir);
-#print Dumper($fs_config);
-#print $fs_config->{section}->{directory}
-#$fs_config_new->XMLout($fs_config, xmldecl=>'<?xml version="1.0">');
-my $t = $fs_config->{section}->{languages}->{'X-PRE-PROCESS'}->[1];
-print $t->{data};
+
+GetOptions("conf=s"  => \$conf_name,
+       "show=s"	       => \$show_names,
+) or usage();
+
+#show_interfaces($show_names)		if ($show_names);
+fs_conf($conf_name) if ($conf_name);
+exit 0;
+
+sub fs_conf_switch {
+    my $config = new Vyatta::Config;
+    #$config->setLevel("system");
+    #print $config->returnValue("domain-name");
+    $config->setLevel("service sip");
+    my $fs_switch = $fs_conf_dir.'/autoload_configs/switch.conf.xml';
+    my $fs_config = XMLin($fs_switch, KeyAttr=>{params=>"+names"});
+    #print Dumper($fs_config);
+    foreach my $key (@{$fs_config->{settings}->{param}}) {
+        if ($key->{name} eq 'dump-cores') {
+            $key->{value}=$config->returnValue("dump-cores");
+        }
+        elsif ($key->{name} eq 'loglevel') {
+            $key->{value}=$config->returnValue("loglevel");
+        }
+        elsif ($key->{name} eq 'max-sessions') {
+            $key->{value}=$config->returnValue("max-sessions");
+        }
+        elsif ($key->{name} eq 'sessions-per-second') {
+            $key->{value}=$config->returnValue("sessions-per-second");
+        }
+        elsif ($key->{name} eq 'rtp-enable-zrtp') {
+            $key->{value}=$config->returnValue("zrtp-secure-media");
+        }
+        
+    }
+    my $fs_config_new = XML::Simple->new(rootname=>'configuration');
+    open my $fh, '>:encoding(UTF-8)', $fs_switch or die "open($fs_switch): $!";
+    $fs_config_new->XMLout($fs_config, OutputFile=>$fh);
+    #print $fs_config_new->XMLout($fs_config, xmldecl=>'<?xml version="1.0">');
+}
+
+sub fs_conf_language {
+    my $config = new Vyatta::Config;
+    $config->setLevel("service sip");
+    my @tmp = $config->returnValues('language');
+    if (scalar(@tmp) > 0) {
+      for my $rem (@tmp) {
+        print $rem;
+      }
+    }
+    print join(",",@tmp)
+    #if (scalar(@{$self->{_remote_host}}) > 0) {
+    #  for my $rem (@{$self->{_remote_host}}) {
+    #    return (undef, '"remote-address" cannot be the same as "remote-host"')
+    #      if ($rem eq $self->{_remote_addr});
+    #  }
+    #}
+    
+    
+}
+
+sub fs_conf {
+    my $name = shift;
+    my $config = new Vyatta::Config;
+    $config->setLevel("service sip");
+    #if (!defined($self->{_secret_file}) && !defined($self->{_tls_def}));
+    if (!defined($config->returnValue("domain-name"))) {
+        return (undef, 'Must specify "domain-name"');
+    }
+    if ($name eq 'switch') {
+        fs_conf_switch();
+    }
+    elsif ($name eq 'lang') {
+        fs_conf_language();
+    }
+    elsif ($name eq 'acl') {
+        fs_conf_switch();
+    }
+}
 
